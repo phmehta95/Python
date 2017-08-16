bffcc1c42f2b235758f5cb0b2cee47d3ee7f20f9
Pruthvi Mehta <qthedesperate@gmail.com>
2017-08-16 09:55:03 +0100

    changed to sort the list

---
+++ median.py
@@ -0,0 +1,11 @@
+def median(lst):
+    n = len(lst)
+    if n < 1:
+            return None
+    if n % 2 == 1:
+            return sorted(lst)[n//2]
+    else:
+            return sum(sorted(lst)[n//2-1:n//2+1])/2.0
+
+
+print (median([-5, -5, -3, -4, 0, -1]))
